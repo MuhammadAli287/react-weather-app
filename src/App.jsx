d61030657104f223156ce735ec19b6b98e13af69
Ali <i.muhammad.ali.287@gmail.com>
2026-09-11 19:59:47 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Weather from "./components/Weather";
 
 export default function App(){
 
     return(
         <>
-        <h1 className="text-3xl text-blue-500">hello world</h1>
-        <p className="text-2xl text-blue-300">welcome to react weather app</p>
+        {/* <h1 className="text-3xl text-blue-500">hello world</h1>
+        <p className="text-2xl text-blue-300">welcome to react weather app</p> */}
+
+        <Weather />
         </>
         
     )
